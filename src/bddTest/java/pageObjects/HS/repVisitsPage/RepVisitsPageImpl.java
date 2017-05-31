839cf61d18bea5e1ff7b1597430141917ca7f8cb
frank.sejas <fsejasm@gmail.com>
2017-05-31 14:33:47 -0400

    Fixed object selection for duplicated class name for Start and End date in the calendar.

--- src/bddTest/java/pageObjects/HS/repVisitsPage/RepVisitsPageImpl.java
+++ src/bddTest/java/pageObjects/HS/repVisitsPage/RepVisitsPageImpl.java
@@ -73,7 +73,8 @@ public class RepVisitsPageImpl extends PageObjectFacadeImpl {
         waitUntilPageFinishLoading();
 
         String startEndDatesForVisitsText = getDriver().findElement(By.cssSelector("div[class='availability _17ehxT_27Cme444W6HfdTN']")).getText();
-        String dayTableText = getDriver().findElement(By.cssSelector("table[class='ui basic table']")).getText();
+        //String dayTableText = getDriver().findElement(By.cssSelector("table[class='ui basic table']")).getText();
+        String dayTableText = getDriver().findElement(By.cssSelector("div[class='_10Tg7oamBO_AGbl5OgX9ba']")).getText();
         String displayStartEndForVisitsText = "2017-2018 Start and End Dates For Visits";
         Assert.assertTrue(dayTableText + " Text is not displayed",
                 startEndDatesForVisitsText.contains("MON TUE WED THU FRI"));
@@ -82,7 +83,7 @@ public class RepVisitsPageImpl extends PageObjectFacadeImpl {
         Assert.assertTrue("Button Start Date is not showing.",
                 button(By.cssSelector("button[class='ui button _1RspRuP-VqMAKdEts1TBAC']")).isDisplayed());
         Assert.assertTrue("Button End Date is not showing.",
-                button(By.cssSelector("button[class='ui button _1RspRuP-VqMAKdEts1TBAC']")).isDisplayed());
+                button(By.cssSelector("button[class='ui button _1RspRuP-VqMAKdEts1TBAC']:nth-child(2)")).isDisplayed());
         Assert.assertTrue("Button Add Time Slot is not showing.",
                 button(By.cssSelector("button[class='ui primary button _3uyuuaqFiFahXZJ-zOb0-w']")).isDisplayed());
     }
