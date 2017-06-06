6605922dd3a353c96e3c4af729ee5c79bb404c1f
bbartizek <brian.bartizek@hobsons.com>
2017-06-06 13:30:54 -0400

    Migrating docker build jobs to new repo

--- docker/run-tests-HE.sh
+++ docker/run-tests-HE.sh
@@ -4,6 +4,7 @@ echo "Invoking Maven Install lifecycle"
 echo "Profile: HE-QA"
 echo "Browser: GridDriver"
 
+mvn -version
+mvn help:active-profiles
 mvn install -P HE-QA -Dbrowser=GridDriver
-
-
+mvn help:active-profiles
