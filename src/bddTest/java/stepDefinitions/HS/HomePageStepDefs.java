bdeb470269a8ec30b3effec5aada46670d968c75
Brian <brian.bartizek@gmail.com>
2017-06-09 15:13:13 -0400

    Create HomePageStepDefs.java

--- src/bddTest/java/stepDefinitions/HS/HomePageStepDefs.java
+++ src/bddTest/java/stepDefinitions/HS/HomePageStepDefs.java
@@ -1,7 +1,6 @@
 package stepDefinitions.HS;
 
 import cucumber.api.java8.En;
-
 import pageObjects.HS.homePage.HomePageImpl;
 
 public class HomePageStepDefs implements En {
@@ -10,13 +9,9 @@ public class HomePageStepDefs implements En {
 
         HomePageImpl homePage = new HomePageImpl();
 
-
         And("^HS I successfully sign out$", homePage::logout );
 
         And("^HS I go to the Counselor Community$", homePage::goToCounselorCommunity);
-
-
-
 
     }
 }
