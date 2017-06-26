5aa7b66572158c899d5d93609840586c5005c116
jorgemaguina <jorge.maguina@truextend.com>
2017-06-26 14:43:24 -0400

    Added 2 new scenarios for MATCH-1811
    - 3 classes updated
    - 1 feature added

--- src/bddTest/java/stepDefinitions/HS/LoginPageStepDefs.java
+++ src/bddTest/java/stepDefinitions/HS/LoginPageStepDefs.java
@@ -2,16 +2,18 @@ package stepDefinitions.HS;
 
 import cucumber.api.java8.En;
 import pageObjects.HS.loginPage.LoginPageImpl;
-import pageObjects.HS.homePage.HomePageImpl;
 
 public class LoginPageStepDefs implements En {
 
     public LoginPageStepDefs() {
 
         LoginPageImpl loginPage = new LoginPageImpl();
-        HomePageImpl homePage = new HomePageImpl();
 
         Given("^HS I am logged in to Intersect HS through Naviance with account \"([^\"]*)\" and username \"([^\"]*)\" and password \"([^\"]*)\"$", loginPage::loginThroughNaviance);
+
+        When("^HS I want to login to the HS app using \"([^\"]*)\" as username and \"([^\"]*)\" as password$", loginPage::login);
+
+        Then("^HS I verify that the HS login page is displayed$", loginPage::verifyHSPage);
 
     }
 }
