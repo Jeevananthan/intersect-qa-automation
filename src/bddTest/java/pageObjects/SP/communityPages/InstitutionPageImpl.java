95cf234824e1798deaa1870dcc49e2a0f5ab0bba
jorgemaguina <jorge.maguina@truextend.com>
2017-07-07 10:58:16 -0400

    Added a waiter for the college name in the college's preview.

--- src/bddTest/java/pageObjects/SP/communityPages/InstitutionPageImpl.java
+++ src/bddTest/java/pageObjects/SP/communityPages/InstitutionPageImpl.java
@@ -19,7 +19,7 @@ public class InstitutionPageImpl extends SeleniumBase {
         link("VIEW NAVIANCE COLLEGE PROFILE").click();
         waitUntilPageFinishLoading();
         getDriver().switchTo().frame(driver.findElement(By.className("IdFjPLV2funrJ0xNAJdsL")));
-        waitUntil(ExpectedConditions.elementToBeClickable(By.cssSelector("h1.masthead__name.ng-binding")));
+        waitUntil(ExpectedConditions.elementToBeClickable(collageNameLabel()));
         Assert.assertTrue("College Name is not displaying in Hubs View", collageNameLabel().getText().trim().equals(collegeName));
         getDriver().switchTo().defaultContent();
     }
