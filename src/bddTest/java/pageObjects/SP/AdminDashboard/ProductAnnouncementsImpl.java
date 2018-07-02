94c29d4306c8c76cb65ddb1bfede25d95c6b97b2
Mauricio Viscarra <mauricio.viscarra@truextend.com>
2018-07-02 13:35:08 -0400

    Removing unused library

--- src/bddTest/java/pageObjects/SP/AdminDashboard/ProductAnnouncementsImpl.java
+++ src/bddTest/java/pageObjects/SP/AdminDashboard/ProductAnnouncementsImpl.java
@@ -2,7 +2,6 @@ package pageObjects.SP.AdminDashboard;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import pageObjects.COMMON.PageObjectFacadeImpl;
