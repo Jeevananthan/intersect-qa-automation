9779e1ebfb50eda64108f41c15e9e7dfd24c6087
Brian <brian.bartizek@gmail.com>
2018-07-16 17:08:57 -0400

    Working on Nightly builds

--- docker/run-tests-Nightly.sh
+++ docker/run-tests-Nightly.sh
@@ -6,6 +6,8 @@ echo "$APIKey"
 
 echo $APIKey
 
+ENV
+
 echo "Calling BDD Tests for SuperMatch"
 curl -H 'Authorization: '"$APIKey"'' https://turbo.hesos.net/v1/apps/26/buildjobs/5635/now/
 echo "Calling BDD Tests for SuperMatch Embedded"
@@ -14,3 +16,5 @@ echo "Calling BDD Tests for SuperMatch2"
 curl -H "Authorization: '$APIKey'" https://turbo.hesos.net/v1/apps/26/buildjobs/5635/now/
 echo "Calling BDD Tests for SuperMatch Embedded2"
 curl -H 'Authorization: "$APIKey"' https://turbo.hesos.net/v1/apps/26/buildjobs/6258/now/
+echo "Calling BDD Tests for SuperMatch Embedded3"
+curl -H "Authorization: {$APIKey}" https://turbo.hesos.net/v1/apps/26/buildjobs/6258/now/
