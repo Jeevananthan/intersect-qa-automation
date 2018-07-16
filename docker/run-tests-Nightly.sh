769a7b1ea2d34a8aa513e99ae81bf5205d4c798b
Brian <brian.bartizek@gmail.com>
2018-07-16 17:36:24 -0400

    Update Nightly build shell script to call all 7 app suites.

--- docker/run-tests-Nightly.sh
+++ docker/run-tests-Nightly.sh
@@ -2,19 +2,17 @@
 
 echo "Invoking CURL requests to run nightly tests"
 
-echo "$APIKey"
-
-echo $APIKey
-
-env
-
+echo "Calling BDD Tests for HE"
+curl -H 'Authorization: '"$APIKey"'' https://turbo.hesos.net/v1/apps/26/buildjobs/4992/now/
+echo "Calling BDD Tests for HS"
+curl -H 'Authorization: '"$APIKey"'' https://turbo.hesos.net/v1/apps/26/buildjobs/4994/now/
+echo "Calling BDD Tests for Support"
+curl -H 'Authorization: '"$APIKey"'' https://turbo.hesos.net/v1/apps/26/buildjobs/4996/now/
+echo "Calling BDD Tests for Community"
+curl -H 'Authorization: '"$APIKey"'' https://turbo.hesos.net/v1/apps/26/buildjobs/4995/now/
 echo "Calling BDD Tests for SuperMatch"
 curl -H 'Authorization: '"$APIKey"'' https://turbo.hesos.net/v1/apps/26/buildjobs/5635/now/
 echo "Calling BDD Tests for SuperMatch Embedded"
-curl -H "Authorization: '"$APIKey"'" https://turbo.hesos.net/v1/apps/26/buildjobs/6258/now/
-echo "Calling BDD Tests for SuperMatch2"
-curl -H "Authorization: '$APIKey'" https://turbo.hesos.net/v1/apps/26/buildjobs/5635/now/
-echo "Calling BDD Tests for SuperMatch Embedded2"
-curl -H 'Authorization: "$APIKey"' https://turbo.hesos.net/v1/apps/26/buildjobs/6258/now/
-echo "Calling BDD Tests for SuperMatch Embedded3"
-curl -H "Authorization: {$APIKey}" https://turbo.hesos.net/v1/apps/26/buildjobs/6258/now/
+curl -H 'Authorization: '"$APIKey"'' https://turbo.hesos.net/v1/apps/26/buildjobs/6258/now/
+echo "Calling BDD Tests for HUBS"
+curl -H 'Authorization: '"$APIKey"'' https://turbo.hesos.net/v1/apps/26/buildjobs/4997/now/
