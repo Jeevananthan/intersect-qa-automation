caa545e850f360f8665dfccbdf6de8b0d1ef68db
Brian <brian.bartizek@gmail.com>
2018-07-20 14:15:17 -0400

    Upped timeout to 60 seconds for Naviance.

--- src/bddTest/java/pageObjects/HS/loginPage/LoginPageImpl.java
+++ src/bddTest/java/pageObjects/HS/loginPage/LoginPageImpl.java
@@ -138,7 +138,7 @@ public class LoginPageImpl extends PageObjectFacadeImpl {
     private void openNavianceLoginPage() {
 
         try {
-            driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+            driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
             driver.executeScript( GetProperties.get("naviance.app.url"));
         } catch (Exception e) {
             getDriver().close();
