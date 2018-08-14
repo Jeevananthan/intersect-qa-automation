4ecca3985f54615b9c8af216a7c591eb25f7b3e0
Natallia Sokh <natalliasokh@Natallias-MacBook-Pro.local>
2018-08-14 14:41:24 +0200

    small refactoring

--- src/bddTest/java/pageObjects/SM/pinnedSchoolsComparePage/PinnedSchoolsComparePageImpl.java
+++ src/bddTest/java/pageObjects/SM/pinnedSchoolsComparePage/PinnedSchoolsComparePageImpl.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import pageObjects.COMMON.PageObjectFacadeImpl;
 
 import java.io.File;
+
 import utilities.HUBSEditMode.Navigation;
 
 import java.util.List;
@@ -19,7 +20,7 @@ public class PinnedSchoolsComparePageImpl extends PageObjectFacadeImpl {
 
     private Logger logger;
     private static String fs = File.separator;
-    private static String propertiesFilePath = String.format(".%ssrc%sbddTest%sresources%sSMTooltipsContent%sSMTooltipsContent.properties",fs ,fs ,fs ,fs ,fs);
+    private static String propertiesFilePath = String.format(".%ssrc%sbddTest%sresources%sSMTooltipsContent%sSMTooltipsContent.properties", fs, fs, fs, fs, fs);
 
     private Navigation navigation = new Navigation();
 
@@ -59,8 +60,8 @@ public class PinnedSchoolsComparePageImpl extends PageObjectFacadeImpl {
 
     public void expandDrawerInPosition(String position) {
         List<WebElement> drawerArrowsList = driver.findElements(By.cssSelector(drawersArrowsLocator));
-        if(drawerArrowsList.get(Integer.parseInt(position)).getAttribute("class").contains("right")) {
-            while(true) {
+        if (drawerArrowsList.get(Integer.parseInt(position)).getAttribute("class").contains("right")) {
+            while (true) {
                 try {
                     drawerArrowsList.get(Integer.parseInt(position)).click();
                     break;
@@ -73,7 +74,7 @@ public class PinnedSchoolsComparePageImpl extends PageObjectFacadeImpl {
 
     public void verifyCollapseExpandButtonTextWithExpandedDrawers(String buttonText, String numberOfExpandedDrawers) {
         List<WebElement> openDrawersList = driver.findElements(By.cssSelector(drawersListLocator + " + tbody"));
-        if(openDrawersList.size() >= Integer.parseInt(numberOfExpandedDrawers)) {
+        if (openDrawersList.size() >= Integer.parseInt(numberOfExpandedDrawers)) {
             Assert.assertTrue("The text in the Collapse/Expand All button is incorrect", collapseExpandAllButton().getText().equals(buttonText));
         }
     }
@@ -82,28 +83,22 @@ public class PinnedSchoolsComparePageImpl extends PageObjectFacadeImpl {
         collapseExpandAllButton().sendKeys(Keys.RETURN);
     }
 
-    // Locators Below
-
-    private String drawersListLocator = "thead.toggle-enabled";
-    private String drawersArrowsLocator = "div.ui.segment.supermatch-compare-content i.caret";
-    private WebElement collapseExpandAllButton() { return driver.findElement(By.cssSelector("button.ui.teal.basic.button[role='button']:not(.icon)")); }
-
     public void verifyTooltipInComparePage(String sectionName) {
         switch (sectionName) {
-            case "Academic Match" :
+            case "Academic Match":
                 verifyTooltipsInCompareScreen("academic.match.in.compare.schools.title");
                 verifyTooltipsInCompareScreen("academic.match.in.compare.schools.labels");
                 verifyTooltipsInCompareScreen("academic.match.in.compare.schools.text.blocks");
                 break;
-            case "Admission Info" :
+            case "Admission Info":
                 verifyTooltipsInCompareScreen("compare.schools.admission.info.titles");
                 verifyTooltipsInCompareScreen("compare.schools.admission.info.text");
                 break;
-            case "Institution Characteristics" :
+            case "Institution Characteristics":
                 verifyTooltipsInCompareScreen("compare.schools.institution.characteristics.titles");
                 verifyTooltipsInCompareScreen("compare.schools.institution.characteristics.text");
                 break;
-            case "Diversity" :
+            case "Diversity":
                 verifyTooltipsInCompareScreen("compare.schools.diversity.titles");
                 verifyTooltipsInCompareScreen("compare.schools.diversity.text");
                 break;
@@ -115,64 +110,64 @@ public class PinnedSchoolsComparePageImpl extends PageObjectFacadeImpl {
         List<WebElement> institutionCharacteristicsIcons = driver.findElements(By.xpath(institutionCharacteristicsLocator));
         List<WebElement> diversityIcons = driver.findElements(By.xpath(diversityTooltipIconsLocator));
         switch (propertiesEntry) {
-            case "academic.match.in.compare.schools.title" :
+            case "academic.match.in.compare.schools.title":
                 academicMatchTooltipIcon().sendKeys(Keys.RETURN);
                 Assert.assertTrue("The title for Academic Match in the Schools Compare screen is not correct",
                         tooltipTitle().getText().trim().equals(getStringFromPropFile(propertiesFilePath, propertiesEntry)));
                 academicMatchTooltipIcon().sendKeys(Keys.RETURN);
                 break;
-            case "academic.match.in.compare.schools.labels" :
+            case "academic.match.in.compare.schools.labels":
                 academicMatchTooltipIcon().sendKeys(Keys.RETURN);
                 List<WebElement> labels = driver.findElements(By.cssSelector(academicMatchLabelsLocator));
-                for(int i = 0; i < labels.size(); i++) {
+                for (int i = 0; i < labels.size(); i++) {
                     Assert.assertTrue("The label for the Academic Match tooltip is not correct",
                             labels.get(i).getText().equals(getListFromPropFile(propertiesFilePath, ";", propertiesEntry).get(i)));
                 }
                 academicMatchTooltipIcon().sendKeys(Keys.RETURN);
                 break;
-            case "academic.match.in.compare.schools.text.blocks" :
+            case "academic.match.in.compare.schools.text.blocks":
                 academicMatchTooltipIcon().sendKeys(Keys.RETURN);
-                for(int i = 0; i < getListFromPropFile(propertiesFilePath, "*", propertiesEntry).size(); i++) {
+                for (int i = 0; i < getListFromPropFile(propertiesFilePath, "*", propertiesEntry).size(); i++) {
                     Assert.assertTrue("The text content in the Academic Match tooltip is not correct",
                             academicMatchTextContent().getText().contains(getListFromPropFile(propertiesFilePath, "*", propertiesEntry).get(i)));
                 }
                 academicMatchTooltipIcon().sendKeys(Keys.RETURN);
                 break;
-            case "compare.schools.admission.info.titles" :
-                for(int i = 0; i < admissionInfoTooltipIcons.size(); i++) {
+            case "compare.schools.admission.info.titles":
+                for (int i = 0; i < admissionInfoTooltipIcons.size(); i++) {
                     admissionInfoTooltipIcons.get(i).sendKeys(Keys.RETURN);
                     Assert.assertTrue("The title for the tooltip in Admission Info is incorrect",
                             tooltipTitle().getText().trim().equals(getListFromPropFile(propertiesFilePath, ";", propertiesEntry).get(i)));
                     admissionInfoTooltipIcons.get(i).sendKeys(Keys.RETURN);
                 }
                 break;
-            case "compare.schools.admission.info.text" :
-                for(int i = 0; i < admissionInfoTooltipIcons.size(); i++) {
+            case "compare.schools.admission.info.text":
+                for (int i = 0; i < admissionInfoTooltipIcons.size(); i++) {
                     admissionInfoTooltipIcons.get(i).sendKeys(Keys.RETURN);
                     Assert.assertTrue("The text for the tooltip in Admission Info is incorrect",
                             academicMatchTextContent().getText().trim().equals(getListFromPropFile(propertiesFilePath, ";", propertiesEntry).get(i)));
                     admissionInfoTooltipIcons.get(i).sendKeys(Keys.RETURN);
                 }
                 break;
-            case "compare.schools.institution.characteristics.titles" :
-                for(int i = 0; i < institutionCharacteristicsIcons.size(); i++) {
+            case "compare.schools.institution.characteristics.titles":
+                for (int i = 0; i < institutionCharacteristicsIcons.size(); i++) {
                     institutionCharacteristicsIcons.get(i).sendKeys(Keys.RETURN);
                     Assert.assertTrue("The title of the tooltip in Institution Characteristics is incorrect",
                             tooltipTitle().getText().trim().equals(getListFromPropFile(propertiesFilePath, ";", propertiesEntry).get(i)));
                     institutionCharacteristicsIcons.get(i).sendKeys(Keys.RETURN);
                 }
                 break;
-            case "compare.schools.institution.characteristics.text" :
-                for(int i = 0; i < institutionCharacteristicsIcons.size(); i++) {
+            case "compare.schools.institution.characteristics.text":
+                for (int i = 0; i < institutionCharacteristicsIcons.size(); i++) {
                     institutionCharacteristicsIcons.get(i).sendKeys(Keys.RETURN);
                     Assert.assertTrue("The text of the tooltip in Institution Characteristics is incorrect. UI: " + academicMatchTextContent().getText() + " Data: "
-                            + getListFromPropFile(propertiesFilePath, ";", propertiesEntry).get(i),
+                                    + getListFromPropFile(propertiesFilePath, ";", propertiesEntry).get(i),
                             academicMatchTextContent().getText().trim().replace("“", "'").replace("”", "'").equals(getListFromPropFile(propertiesFilePath, ";", propertiesEntry).get(i)));
                     institutionCharacteristicsIcons.get(i).sendKeys(Keys.RETURN);
                 }
                 break;
-            case "compare.schools.diversity.titles" :
-                for(int i = 0; i < diversityIcons.size(); i++) {
+            case "compare.schools.diversity.titles":
+                for (int i = 0; i < diversityIcons.size(); i++) {
                     diversityIcons.get(i).sendKeys(Keys.RETURN);
                     Assert.assertTrue("The title of the tooltip in Diversity is incorrect",
                             tooltipTitle().getText().trim().equals(getListFromPropFile(propertiesFilePath, ";", propertiesEntry).get(i)));
@@ -182,38 +177,27 @@ public class PinnedSchoolsComparePageImpl extends PageObjectFacadeImpl {
         }
     }
 
-    // Locators Below
-
-    private String admissionInfoTooltipIconsLocator = "//th[text() = 'Admission Info']/../../../tbody/tr/td/div/div/button";
-    private String institutionCharacteristicsLocator = "//th[text() = 'Institution Characteristics']/../../../tbody/tr/td/div/div/button";
-    private String diversityTooltipIconsLocator = "//th[text() = 'Diversity']/../../../tbody/tr/td/div/div/button";
-    private WebElement tooltipTitle() { return driver.findElement(By.cssSelector("div[id *= 'supermatch-tooltip-'] div.header")); }
-    private WebElement tooltipText() { return driver.findElement(By.cssSelector("div[class$='very wide inverted popup transition visible'] div.content")); }
-    private String academicMatchLabelsLocator = "div[id *= 'supermatch-tooltip-'] div.content b";
-    private WebElement academicMatchTextContent() { return driver.findElement(By.cssSelector("div[id *= 'supermatch-tooltip-'] div.content")); }
-    private WebElement academicMatchTooltipIcon() { return driver.findElement(By.cssSelector("td[aria-label=\" Academic Match\"] button")); }
-
     public void verifyHousingInfoIsDisplayedAfterClickingSections(DataTable dataTable) {
         List<String> details = dataTable.asList(String.class);
-        for(String element : details) {
+        for (String element : details) {
             switch (element) {
-                case "# of 4 year majors" :
+                case "# of 4 year majors":
                     number4YearMajorsLink().sendKeys(Keys.RETURN);
                     verifyStudiesTab();
                     break;
-                case "Varsity sports" :
+                case "Varsity sports":
                     varsitySportsLink().sendKeys(Keys.RETURN);
                     verifyHousingInfoPage();
                     break;
-                case "Male varsity sports" :
+                case "Male varsity sports":
                     maleSportsLink().click();
                     verifyHousingInfoPage();
                     break;
-                case "Female varsity sports" :
+                case "Female varsity sports":
                     femaleSportsLink().click();
                     verifyHousingInfoPage();
                     break;
-                case "Clubs and organizations" :
+                case "Clubs and organizations":
                     clubsAndOrganizationsLink().sendKeys(Keys.RETURN);
                     verifyHousingInfoPage();
                     break;
@@ -241,7 +225,7 @@ public class PinnedSchoolsComparePageImpl extends PageObjectFacadeImpl {
     }
 
     public void unpinSchool(String collegeName) {
-        if(pinLink(collegeName).getText().contains("PINNED")) {
+        if (pinLink(collegeName).getText().contains("PINNED")) {
             pinLink(collegeName).click();
             waitUntil(ExpectedConditions.numberOfElementsToBe(By.xpath(pinLinkLocator(collegeName)), 0));
             waitUntilPageFinishLoading();
@@ -252,14 +236,74 @@ public class PinnedSchoolsComparePageImpl extends PageObjectFacadeImpl {
         exportButton().click();
     }
 
-    private WebElement number4YearMajorsLink() { return driver.findElement(By.cssSelector("td[aria-label=\"Academics # of 4 Year Majors\"] + td a")); }
-    private WebElement majorsHeaderInStudiesTab() { return driver.findElement(By.cssSelector("h3.ng-binding")); }
-    private WebElement varsitySportsLink() { return driver.findElement(By.cssSelector("td[aria-label=\"Athletics Levels Available\"] + td > a")); }
-    private WebElement housingInfoSection() { return driver.findElement(By.cssSelector("div.student-life-housing-information__header.fc-grid__col.fc-grid__col--xs-12.fc-grid__col--sm-7.fc-grid__col--md-12 h2")); }
-    private WebElement maleSportsLink() { return driver.findElement(By.cssSelector("td[aria-label=\"Athletics Levels Available\"] + td ul li:nth-of-type(1)")); }
-    private WebElement femaleSportsLink() { return driver.findElement(By.cssSelector("td[aria-label=\"Athletics Levels Available\"] + td ul li:nth-of-type(2)")); }
-    private WebElement clubsAndOrganizationsLink() { return driver.findElement(By.cssSelector("td[aria-label=\"Student Life Clubs & Organizations\"] + td a")); }
-    private WebElement pinLink(String collegeName) { return driver.findElement(By.xpath(pinLinkLocator(collegeName))); }
-    private String pinLinkLocator(String collegeName) { return "//p[@class='collegename' and text() = '" + collegeName + "']/../p/a/span[@class = 'supermatch-toggle-icon supermatch-college-button-selected']"; }
-    private WebElement exportButton() { return driver.findElement(By.cssSelector("a.ui.teal.basic.button")); }
+    // Locators Below
+
+    private String drawersListLocator = "thead.toggle-enabled";
+    private String drawersArrowsLocator = "div.ui.segment.supermatch-compare-content i.caret";
+
+    private WebElement collapseExpandAllButton() {
+        return driver.findElement(By.cssSelector("button.ui.teal.basic.button[role='button']:not(.icon)"));
+    }
+
+    private String admissionInfoTooltipIconsLocator = "//th[text() = 'Admission Info']/../../../tbody/tr/td/div/div/button";
+    private String institutionCharacteristicsLocator = "//th[text() = 'Institution Characteristics']/../../../tbody/tr/td/div/div/button";
+    private String diversityTooltipIconsLocator = "//th[text() = 'Diversity']/../../../tbody/tr/td/div/div/button";
+
+    private WebElement tooltipTitle() {
+        return driver.findElement(By.cssSelector("div[id *= 'supermatch-tooltip-'] div.header"));
+    }
+
+    private WebElement tooltipText() {
+        return driver.findElement(By.cssSelector("div[class$='very wide inverted popup transition visible'] div.content"));
+    }
+
+    private String academicMatchLabelsLocator = "div[id *= 'supermatch-tooltip-'] div.content b";
+
+    private WebElement academicMatchTextContent() {
+        return driver.findElement(By.cssSelector("div[id *= 'supermatch-tooltip-'] div.content"));
+    }
+
+    private WebElement academicMatchTooltipIcon() {
+        return driver.findElement(By.cssSelector("td[aria-label=\" Academic Match\"] button"));
+    }
+
+    private WebElement number4YearMajorsLink() {
+        return driver.findElement(By.cssSelector("td[aria-label=\"Academics # of 4 Year Majors\"] + td a"));
+    }
+
+    private WebElement majorsHeaderInStudiesTab() {
+        return driver.findElement(By.cssSelector("h3.ng-binding"));
+    }
+
+    private WebElement varsitySportsLink() {
+        return driver.findElement(By.cssSelector("td[aria-label=\"Athletics Levels Available\"] + td > a"));
+    }
+
+    private WebElement housingInfoSection() {
+        return driver.findElement(By.cssSelector("div.student-life-housing-information__header.fc-grid__col.fc-grid__col--xs-12.fc-grid__col--sm-7.fc-grid__col--md-12 h2"));
+    }
+
+    private WebElement maleSportsLink() {
+        return driver.findElement(By.cssSelector("td[aria-label=\"Athletics Levels Available\"] + td ul li:nth-of-type(1)"));
+    }
+
+    private WebElement femaleSportsLink() {
+        return driver.findElement(By.cssSelector("td[aria-label=\"Athletics Levels Available\"] + td ul li:nth-of-type(2)"));
+    }
+
+    private WebElement clubsAndOrganizationsLink() {
+        return driver.findElement(By.cssSelector("td[aria-label=\"Student Life Clubs & Organizations\"] + td a"));
+    }
+
+    private WebElement pinLink(String collegeName) {
+        return driver.findElement(By.xpath(pinLinkLocator(collegeName)));
+    }
+
+    private String pinLinkLocator(String collegeName) {
+        return "//p[@class='collegename' and text() = '" + collegeName + "']/../p/a/span[@class = 'supermatch-toggle-icon supermatch-college-button-selected']";
+    }
+
+    private WebElement exportButton() {
+        return driver.findElement(By.cssSelector("a.ui.teal.basic.button"));
+    }
 }
