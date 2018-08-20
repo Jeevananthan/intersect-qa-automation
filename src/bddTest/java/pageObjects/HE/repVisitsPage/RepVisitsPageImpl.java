22fe255ea7a094d24c2040d8bdb34ebc2a38e9ca
Mauricio.Viscarra <mauricio.viscarra@hobsons.com>
2018-08-20 15:08:36 -0400

    Adding 3062 fix

--- src/bddTest/java/pageObjects/HE/repVisitsPage/RepVisitsPageImpl.java
+++ src/bddTest/java/pageObjects/HE/repVisitsPage/RepVisitsPageImpl.java
@@ -2228,8 +2228,10 @@ public class RepVisitsPageImpl extends PageObjectFacadeImpl {
     public void verifyPasswordFields(String AccountInformationPage,String firstName,String lastName,String eMail,DataTable dataTable) {
         String details[] = AccountInformationPage.split(",");
         for(int i=0;i<details.length-1;i++) {
-            Assert.assertTrue(details[i] + " is not showing.", text(details[i]).isDisplayed());}
+            Assert.assertTrue(details[i] + " is not showing.",
+                    driver.findElement(By.xpath("//span[text()='"+details[i]+"']")).isDisplayed());}
         currentPasswordInput().sendKeys(Keys.PAGE_DOWN);
+
         List<String> list = dataTable.asList(String.class);
         for (String passwordCriteria : list) {
             Assert.assertTrue(passwordCriteria + " is not showing.",text(passwordCriteria).isDisplayed());
@@ -2338,6 +2340,7 @@ public class RepVisitsPageImpl extends PageObjectFacadeImpl {
     public void verifySuccessMessageinAccountSettingsPage(String message){
         String SuccessMessage = driver.findElement(By.xpath("//span[@class='LkKQEXqh0w8bxd1kyg0Mq']/span")).getText();
         Assert.assertTrue("Success message is not displayed",message.equals(SuccessMessage));
+        waitUntil(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//span[@class='LkKQEXqh0w8bxd1kyg0Mq']/sp")));
     }
 
     public void selectCalendar() {
@@ -3779,7 +3782,7 @@ public class RepVisitsPageImpl extends PageObjectFacadeImpl {
         return text;
     }
     private WebElement emailTextBox(){
-        WebElement text=driver.findElement(By.id("am_notification_contacts_additional_emails"));
+        WebElement text=driver.findElement(By.id("user-form-email"));
         return text;
     }
 
