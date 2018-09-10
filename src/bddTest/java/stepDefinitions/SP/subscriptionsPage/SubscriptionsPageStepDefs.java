a89843a727d9eccb07017fda9e9d475f7a884c35
jorgemaguina <jorge.maguina@truextend.com>
2018-09-10 15:19:21 -0400

    -  More specific steps were added. -  the verification steps is more explicit. - Generic methods (such as button(String string)) are being used whenever is possible.

--- src/bddTest/java/stepDefinitions/SP/subscriptionsPage/SubscriptionsPageStepDefs.java
+++ src/bddTest/java/stepDefinitions/SP/subscriptionsPage/SubscriptionsPageStepDefs.java
@@ -9,14 +9,20 @@ public class SubscriptionsPageStepDefs implements En {
 
         SubscriptionsPageImpl subscriptionsPage = new SubscriptionsPageImpl();
 
-        And("^SP I add a new subscription of type \"([^\"]*)\"$", subscriptionsPage::addNewSubscription);
-
         Then("^SP I verify the functionality of the Back button$", subscriptionsPage::verifyBackButtonFunctionality);
 
         And("^SP I fill the new subscription with the following data:$", subscriptionsPage::fillNewSubscriptionForm);
 
         And("^SP I save the new subscription$", subscriptionsPage::clickFinish);
 
-        Then("^SP I verify that a new subscription was added with the following data:$", subscriptionsPage::verifyNewSubscription);
+        Then("^SP I verify that a new subscription was added to the subscription table with the following data:$", subscriptionsPage::verifyNewSubscription);
+
+        And("^SP I click the Next button$", subscriptionsPage::clickNextButton);
+
+        And("^SP I select the radio button \"([^\"]*)\" in Add new Subscription modal$", subscriptionsPage::selectRadioButton);
+
+        And("^SP I click the button \"([^\"]*)\"$", subscriptionsPage::clickButton);
+
+        And("^SP I delete the subscriptions with the following data:$", subscriptionsPage::deleteSubscription);
     }
 }
