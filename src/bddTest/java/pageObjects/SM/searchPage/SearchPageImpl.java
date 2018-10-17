46a75adb254d0b7d915313cba931b90c1b60a4e0
Jeeva <jeeva.c@indiumsoft.com>
2018-10-17 13:27:42 -0400

    Updated locator for global search

--- src/bddTest/java/pageObjects/SM/searchPage/SearchPageImpl.java
+++ src/bddTest/java/pageObjects/SM/searchPage/SearchPageImpl.java
@@ -2247,7 +2247,7 @@ public class SearchPageImpl extends PageObjectFacadeImpl {
 
     public void checkNumberOfElementsDisplayed(Integer number, String locator){
 
-        waitUntilPageFinishLoading();
+       waitUntilPageFinishLoading();
        Assert.assertEquals((Integer) driver.findElements(By.cssSelector(locator)).size(), number);
 
     }
