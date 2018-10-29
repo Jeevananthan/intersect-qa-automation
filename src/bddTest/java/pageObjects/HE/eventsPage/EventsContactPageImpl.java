51ac57a2582b0f8b4732ebdcc868f2a59beb32ee
bhangumandeep <mandeep.bhangu@hobsons.com>
2018-10-29 13:34:54 -0400

    MATCH-2912 - Fixed failed test cases

--- src/bddTest/java/pageObjects/HE/eventsPage/EventsContactPageImpl.java
+++ src/bddTest/java/pageObjects/HE/eventsPage/EventsContactPageImpl.java
@@ -72,7 +72,7 @@ public class EventsContactPageImpl extends PageObjectFacadeImpl {
         returnToListLink().sendKeys(Keys.RETURN);
     }
     public void verifyContact(String contact){
-        waitUntilPageFinishLoading();
+        waitForUITransition();
        String results = driver.findElement(By.cssSelector("input[name='contacts-search']")).getAttribute("value");
        Assert.assertTrue("Newly created contact is not present", results.equals(contact));
 
@@ -80,7 +80,7 @@ public class EventsContactPageImpl extends PageObjectFacadeImpl {
     public  void requiredFieldMessage(){
         String message = driver.findElement(By.cssSelector("div._1ryO_t8iwDyfipN2lExls span")).getText();
         Assert.assertTrue("Contact Name require field message is not displayed",contactMessage().isDisplayed());
-        eventsTitle().click();
+
     }
     public void editEventContact(){
         searchNewContactField().click();
